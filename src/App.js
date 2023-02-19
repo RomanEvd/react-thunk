af77d18da191b6791488fcbe096fdbf7c68137bb
RomanEvd <evdokimov.roman.1993@mail.ru>
2023-02-19 19:40:49 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Provider } from "react-redux";
 
-function App() {
+import "./App.css";
+import store from "./store";
+import Task1 from "./components/Task1";
+import Task2 from "./components/Task2";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Task1 />
+      <Task2 />
+    </Provider>
   );
 }
-
-export default App;
